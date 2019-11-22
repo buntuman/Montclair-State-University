a58ff05c629c2aeed2559f0c3d0fb02e58d9bea0
buntuman <danielgitahi5@gmail.com>
2019-11-21 23:20:39 -0500

    Added program descriprion at the top

--- Classes/Intro-to-Computer-Networks/Client-Server-APP/TCP/TCPStringPersistentMultithread.py
+++ Classes/Intro-to-Computer-Networks/Client-Server-APP/TCP/TCPStringPersistentMultithread.py
@@ -1,8 +1,11 @@
+# client server application simulating persistent TCP
+#This program also utilizes multithreading 
 from socket import *
 import threading
 
 
-def serveClient(tcpConnectionSock):
+def serveClient(tcpConnectionSock,clientCount):
+    print("Serving Client %d "%(clientCount))
     while True:
         sentence = tcpConnectionSock.recv(1024).decode()
         if(sentence.lower() == 'quit'):
@@ -10,24 +13,27 @@ def serveClient(tcpConnectionSock):
         else:
             capitalizedSentence = sentence.upper()
             tcpConnectionSock.send(capitalizedSentence.encode())
+    print(" Client %d  Requested quit. Bye client %d :) "%(clientCount,clientCount))
 
 
 class ServerThread(threading.Thread):
-    def __init__(self,tcpConnectionSocket):
+    def __init__(self,tcpConnectionSocket,clientNum):
         self.tcpConnectionSocket = tcpConnectionSocket
+        self.clientNum = clientNum
         threading.Thread.__init__(self)
 
     def run(self):
-        serveClient(self.tcpConnectionSocket)
+        serveClient(self.tcpConnectionSocket,self.clientNum)
 
-
+clientCount = 0
 serverPort = 5000
 serverSocket = socket(AF_INET, SOCK_STREAM)
 serverSocket.bind(('', serverPort))
 serverSocket.listen(1)
 while True:
     print("The server is ready to receive.")
+    clientCount += 1 # client counter. Not neccessary. Its just for my own sanity. Lol
     connectionSocket, addr = serverSocket.accept()
-    t = ServerThread(connectionSocket)
+    t = ServerThread(connectionSocket,clientCount)
     t.start()
 connectionSocket.close()
