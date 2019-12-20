b3cc36d2593ad49c2c0f8fac1e1d95fb225825e2
buntuman <danielgitahi5@gmail.com>
2019-12-19 21:59:07 -0500

    Fixed typos

--- Classes/Intro-to-Computer-Networks/Client-Server-APP/TCP/TCPClient.py
+++ Classes/Intro-to-Computer-Networks/Client-Server-APP/TCP/TCPClient.py
@@ -2,14 +2,13 @@ from socket import *
 
 serverName = 'localhost'
 serverPort = 5000
-while True:
+
+sentence = input("Enter Math Expression : ")
+while sentence.lower() != 'quit':
     clientSocket = socket(AF_INET, SOCK_STREAM)
     clientSocket.connect((serverName, serverPort))
-    sentence = input("Input a lowercase sentence: ")
-    if(sentence.lower() == 'quit'):
-        clientSocket.close()
-        break;
     clientSocket.send(sentence.encode())
     modifiedSentence = clientSocket.recv(1024)
     print(modifiedSentence.decode())
     clientSocket.close()
+    sentence = input("Enter Math Expression : ")
--- Classes/Intro-to-Computer-Networks/Client-Server-APP/TCP/TCPServer.py
+++ Classes/Intro-to-Computer-Networks/Client-Server-APP/TCP/TCPServer.py
@@ -2,12 +2,11 @@ from socket import *
 
 serverPort = 5000
 serverSocket = socket(AF_INET, SOCK_STREAM)
-serverSocket.bind(('', serverPort))
-serverSocket.listen(1)
+serverSocket.bind(("", serverPort))
+serverSocket.listen(5)
 print("The server is ready to receive.")
 while True:
     connectionSocket, addr = serverSocket.accept()
     sentence = connectionSocket.recv(1024).decode()
-    capitalizedSentence = sentence.upper()
-    connectionSocket.send(capitalizedSentence.encode())
+    connectionSocket.send(str(eval(sentence)).encode())
     connectionSocket.close()
